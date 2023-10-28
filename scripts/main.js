258ead397dcf3703f45aa13f196b14fcaec9418a
Igor Mykhalchuk <mykhalchuk.i.a@gmail.com>
2023-10-27 20:25:54 -0400

    show / hide contacts  in mobile added

--- scripts/main.js
+++ scripts/main.js
@@ -1,36 +1,18 @@
+let flag = 0;
 
-function showMore() {
-   const inDetails = document.getElementById("show-more-details")
-   const firstArticle=document.getElementById("first-article");
-   const secondArticle=document.getElementById("second-article");
-   const showMoreDetails=document.getElementById("show-more-details"); 
-   const firstGoBackLink=document.getElementById("first-go-back-link");
+function showHideContacts() {
+   const contacts = document.getElementById("contacts-item");
+   const getInTouch = document.getElementById("get-in-touch");
+
+   if(flag === 0) {
+      contacts.style.backgroundColor = "hsla(345, 68%, 30%, 0.8)";
+      getInTouch.style.display = "flex";
+      flag = 1;
+   } else {
+      getInTouch.style.display = "none";
+      contacts.style.backgroundColor = "initial";
+      flag = 0;
+   }
 
 
-   showMoreDetails.style.display="none";
-   firstGoBackLink.style.display="none";
-   firstArticle.style.marginBottom= 0;
-   firstArticle.style.paddingBottom= "2rem";
-   firstArticle.style.backgroundColor="rgba(235, 235, 235, 0.85)";
-   firstArticle.style.border="1px solid rgba(204, 204, 204)";
-   secondArticle.style.marginTop= 0;
-   secondArticle.style.paddingTop= "1.5rem";
-   secondArticle.style.display="block";
 }
-
-function showLess() {
-   const firstArticle=document.getElementById("first-article");
-   const secondArticle=document.getElementById("second-article");
-   const showMoreDetails=document.getElementById("show-more-details"); 
-   const firstGoBackLink=document.getElementById("first-go-back-link");
-   
-   firstArticle.style.display="block";
-   secondArticle.style.display="none";
-   showMoreDetails.style.display="block";
-   firstGoBackLink.style.display="block";
-   firstArticle.style.marginBottom= "4rem";
-   firstArticle.style.paddingBottom= "5rem";
-   firstArticle.style.backgroundColor="rgba(255, 255, 255, 0.85)";
-   firstArticle.style.border="none";
-}
-
